2371658e70c6e6be2718b1633f9195919b374a73
Rudzeris <ruzil1atangulov@gmail.com>
2025-05-18 19:59:02 +1000

    Добавили ограничение к вращению камеры

--- Assets/Spripts/PlayerRotate.cs
+++ Assets/Spripts/PlayerRotate.cs
@@ -3,22 +3,32 @@ using UnityEngine;
 public enum CameraRotate { None, X, Y }
 public class PlayerRotate : MonoBehaviour
 {
+    // Добавляя скрипт для игрока, выберите X. Для камеры - Y
     public CameraRotate CameraRotate = CameraRotate.None;
     public float SpeedRotation = 10f;
+    public float maxAngle = 45f;
+    public float minAngle = -45f;
+    private float currentAngle = 0f;
     // Update is called once per frame
     void Update()
     {
-        float x = 0;
         float y = 0;
+        float x = 0;
         switch (CameraRotate)
         {
             case CameraRotate.X:
-                x = Input.GetAxis("Mouse X") * Time.deltaTime * SpeedRotation;
+                y = Input.GetAxis("Mouse X") * Time.deltaTime * SpeedRotation;
                 break;
             case CameraRotate.Y:
-                y = -Input.GetAxis("Mouse Y") * Time.deltaTime * SpeedRotation;
+                x = -Input.GetAxis("Mouse Y") * Time.deltaTime * SpeedRotation;
+                currentAngle = Mathf.Clamp(currentAngle + x, minAngle, maxAngle);
                 break;
         }
-        transform.Rotate(y, x, 0);
+        transform.localEulerAngles = new Vector3(
+                    currentAngle,
+                    transform.localEulerAngles.y,
+                    transform.localEulerAngles.z
+                    );
+        transform.Rotate(0, y, 0); // вращение влево вправо
     }
 }
