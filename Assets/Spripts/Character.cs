a556bd3f9b90e1e94c6efe9e067973217448e775
Rudzeris <ruzil1atangulov@gmail.com>
2025-05-18 20:27:15 +1000

    Добавили HP для игрока

---
+++ Assets/Spripts/Character.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float HP = 100f;
+    private bool isDead = false;
+   
+    public void GetDamage(float damage)
+    {
+        if (isDead) return;
+        HP -= damage;
+        if(HP <= 0)
+        {
+            isDead = true;
+            HP = 0;
+            Debug.Log("Character is dead");
+        }
+    }
+}
