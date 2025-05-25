b22c6b9bd210d116627c8ae6c5597fc83f5499b4
Rudzeris <ruzil1atangulov@gmail.com>
2025-05-25 20:44:00 +1000

    #3 Добавили атаку для игрока и "AI" для противника

--- Assets/Spripts/Character.cs
+++ Assets/Spripts/Character.cs
@@ -1,26 +1,40 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Character : MonoBehaviour
 {
     public float HP = 100f;
+    public GameObject Camera;
     public GameObject GameOverPanel;
     private bool isDead = false;
-    void Update()
-    {
-        GetDamage(10*Time.deltaTime);
-    }
+
     public void GetDamage(float damage)
     {
         if (isDead) return;
         HP -= damage;
-        if(HP <= 0)
+        if (HP <= 0)
         {
             isDead = true;
             HP = 0;
             Debug.Log("Character is dead");
-            GameOverPanel.SetActive(true);
-            Destroy(gameObject);
+            if(GameOverPanel != null)
+                GameOverPanel.SetActive(true);
+            StartCoroutine(Die());
         }
+    }
+    private IEnumerator Die()
+    {
+        if(GetComponent<WanderingAI>() is WanderingAI ai)
+            ai.enabled = false;
+        transform.Rotate(-75, 0, 0);
+        yield return new WaitForSeconds(1.5f);
+        if (Camera != null)
+        {
+            Camera.transform.parent = null;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        
+        Destroy(this.gameObject);
     }
 }
---
+++ Assets/Spripts/RayShooter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+public class RayShooter : MonoBehaviour
+{
+    private Camera _camera;
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(
+              _camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+            Ray ray = _camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //StartCoroutine(SphereIndicator(hit.point));
+                Character enemy = hit.collider?.GetComponent<Character>();
+                if(enemy != null)
+                    enemy.GetDamage(50);
+            }
+        }
+    }
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
+    private void OnGUI()
+    {
+        int size = 12;
+        float x = _camera.pixelWidth / 2 - size / 4;
+        float y = _camera.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(x, y, size, size), "*");
+    }
+}
