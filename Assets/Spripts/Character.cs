9b264310fa1bab226a03aa848deade98d30af5d9
Rudzeris <ruzil1atangulov@gmail.com>
2025-05-25 17:26:26 +1000

    #3 До 1-й практики: создан UI смерти и кнопка перезапуска сцены

--- Assets/Spripts/Character.cs
+++ Assets/Spripts/Character.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Character : MonoBehaviour
 {
     public float HP = 100f;
+    public GameObject GameOverPanel;
     private bool isDead = false;
     void Update()
     {
@@ -17,7 +19,13 @@ public class Character : MonoBehaviour
             isDead = true;
             HP = 0;
             Debug.Log("Character is dead");
+            GameOverPanel.SetActive(true);
             Destroy(gameObject);
         }
+    }
+    public void ReloadGame()
+    {
+        //Debug.Log("Reloading game...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
